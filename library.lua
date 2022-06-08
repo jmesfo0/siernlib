3f5640736b32a26d41a4eeb0f5c3b3152ea34b52
James Y <42681518+jmesfo0@users.noreply.github.com>
2022-06-08 16:02:52 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -725,10 +725,8 @@
                         tog = not tog
                         callback(tog)
                         if tog then
-                            game:GetService('TweenService'):Create(status2, TweenInfo.new(0.2), {BackgroundColor3 = Color3.fromRGB(31, 152, 82)}):Play()
                             game:GetService('TweenService'):Create(uIStroke1, TweenInfo.new(0.2), {Color = Color3.fromRGB(52, 255, 137)}):Play()
                         else
-                            game:GetService('TweenService'):Create(status2, TweenInfo.new(0.2), {BackgroundColor3 = Color3.fromRGB(38, 38, 38)}):Play()
                             game:GetService('TweenService'):Create(uIStroke1, TweenInfo.new(0.2), {Color = Color3.fromRGB(48, 48, 48)}):Play()
                         end
                     end)
