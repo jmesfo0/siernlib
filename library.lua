db2de7818b8bbdc316409205e10b201ee0bce5cb
James Y <42681518+jmesfo0@users.noreply.github.com>
2022-06-08 15:28:26 -0700

    Update library.lua

--- library.lua
+++ library.lua
@@ -723,15 +723,14 @@
 
                     textButton1.MouseButton1Click:Connect(function()
                         tog = not tog
+                        callback(tog)
                         
                         if tog then
                             game:GetService('TweenService'):Create(status2, TweenInfo.new(0.2), {BackgroundColor3 = Color3.fromRGB(31, 152, 82)}):Play()
                             game:GetService('TweenService'):Create(uIStroke1, TweenInfo.new(0.2), {Color = Color3.fromRGB(52, 255, 137)}):Play()
-                            callback(tog)
                         else
                             game:GetService('TweenService'):Create(status2, TweenInfo.new(0.2), {BackgroundColor3 = Color3.fromRGB(38, 38, 38)}):Play()
                             game:GetService('TweenService'):Create(uIStroke1, TweenInfo.new(0.2), {Color = Color3.fromRGB(48, 48, 48)}):Play()
-                            callback(tog)
                         end
                         
                     end)
